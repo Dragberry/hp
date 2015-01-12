61e99035123ca1f020b66358734e45a519c5dd64
Maksim Drahun <DRAHUN.MAXIM@standardbank.co.za>
2015-01-12 15:10:54 +0300

    #11 Pretty URLs

--- happytime-web/src/main/java/by/happytime/contoller/ProductController.java
+++ happytime-web/src/main/java/by/happytime/contoller/ProductController.java
@@ -1,14 +1,34 @@
 package by.happytime.contoller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+
+import by.happytime.domain.Product;
+import by.happytime.repository.ProductRepo;
 
 @ManagedBean(name = "productController")
 @RequestScoped
 public class ProductController implements Serializable {
 
     private static final long serialVersionUID = -3391995776641623083L;
+    
+    @ManagedProperty("#{productRepo}")
+	private ProductRepo productRepo;
 
+    public List<Product> getProductList() {
+    	return productRepo.findAll();
+    }
+    
+	public ProductRepo getProductRepo() {
+		return productRepo;
+	}
+
+	public void setProductRepo(ProductRepo productRepo) {
+		this.productRepo = productRepo;
+	}
+    
 }
